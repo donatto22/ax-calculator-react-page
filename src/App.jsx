6472553f390a7c8647f99469921f6196878940bf
donatto22 <ottanod22@gmail.com>
2023-09-01 23:54:07 -0500

    se agrega el botón para apoyar de sponsor

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { Box, Image } from '@chakra-ui/react'
 import Headers from './components/Header'
 
+import { useEffect } from 'react'
+
+// aos js
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import GitHubButton from 'react-github-btn'
+
 const App = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
     return (
         <>
         <Headers />
 
-        <Box w='100vw' height='100vh' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
-            <Box w='180px' mb='2em'>
+        <Box w='100vw' height='100vh' display='flex' gap='2em' flexDirection='column' alignItems='center' justifyContent='center'>
+            <Box w='180px' data-aos="fade-up" data-aos-duration="1000">
                 <Image src='./logo.png' alt='logo ax calculator' />
             </Box>
 
-            <Box fontSize='34px'>
+            <Box fontSize='34px' data-aos="fade-up" data-aos-duration="1200">
                 4x Calculator
             </Box>
+
+            <Box data-aos="fade-up" data-aos-duration="1400">
+                <Box transform='scale(1.2)'>
+                    <GitHubButton href="https://github.com/sponsors/donatto22" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-heart" data-size="large" aria-label="Sponsor @donatto22 on GitHub">Sponsor</GitHubButton>
+                </Box>
+            </Box>            
         </Box>
         </>
     )
